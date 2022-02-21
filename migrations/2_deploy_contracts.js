d258ce9fcc84d656dce7702927db7a40ab7ad4f6
grsLammy <grspro98@gmail.com>
2022-02-21 19:33:47 +0530

    testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,24 @@
-const Tether = artifacts.require("Tether");
-const RewardToken = artifacts.require("RewardToken");
-const DecentralBank = artifacts.require("DecentralBank");
+const RewardToken = artifacts.require('RewardToken')
+const Tether = artifacts.require('Tether')
+const DecentralBank = artifacts.require('DecentralBank')
 
-module.exports = async function (deployer, network, accounts) {
-  //deploy mock Tether contract
+module.exports = async function(deployer, network, accounts) {
+  
+  // Deploy Mock Tether Token
   await deployer.deploy(Tether)
   const tether = await Tether.deployed()
 
-  //deploy RewardToken contract
+  // Deploy RewardToken Token
   await deployer.deploy(RewardToken)
   const rewardToken = await RewardToken.deployed()
 
-  //deploy DecentralBank contract
+  // Deploy DecentralBank
   await deployer.deploy(DecentralBank, rewardToken.address, tether.address)
   const decentralBank = await DecentralBank.deployed()
 
-  //Transfer RewardToken to Decentral Bank
+  // Transfer all tokens to DecentralBank (1 million)
   await rewardToken.transfer(decentralBank.address, '1000000000000000000000000')
 
-  //Distribute 100 Tether tokens to investor
-  await tether.transfer(accounts[1],'100000000000000000000')
-};
+  // Transfer 100 Mock Tether tokens to investor
+  await tether.transfer(accounts[1], '100000000000000000000')
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ class Navbar extends Component {
     render() {
         return(
             <nav className = 'navbar navbar-dark fixed-top shadow p-0' style={{backgroundColor:'black', height:'50px'}}>
-                <a className='navbar-brand col-sm-3 col-md-2 mr-0'
+                <a href =" " className='navbar-brand col-sm-3 col-md-2 mr-0'
                 style={{color:'white'}}>
-                <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='bank image'/>
+                <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='bank'/>
                 DAPP Yield Staking (Decentralized Banking)
                 </a>
                 <ul className='navbar-nav px-3'>
